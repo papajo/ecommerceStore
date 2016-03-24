bf33acb941aa0bfa2d7e1e6f599a234bce20f47a
papajo <joshipv2@gmail.com>
2016-03-24 07:16:55 -0400

    Add morgan for login setup user schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+/* user schema attributes */
+var UserSchema = new Schema({
+    email: {type: String, unique: true, lowercase: true},
+    password: String,
+
+    profile: {
+        name: {type: String, default: ''},
+        picture: {type: String, default: ''}
+    },
+    
+    address: String,
+    history: [{
+        date: Date,
+        paid: {type: Number, default: 0},
+        /* item:{Type: Schema.Types.ObjectId, ref: ''} */
+    }]
+
+});
+
+/* Hash the password before saving to DB */
+UserSchema.pre('save', function(next){
+    var user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt){
+        if(err) return next(err);
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+/* Compare passwords */
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
+
+//Middlware
+app.use(morgan('dev'));
+
+//Routes
+app.get('/', function(req, res){
+    res.json('Hello Jason!');
+});
+
+app.get('/name', function (req, res) {
+    res.json('Hello! ' + req.name);
+})
+
+
 
 app.listen(3000, function (err) {
     if (err) throw err;
