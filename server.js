0cea2a91990e268260a48b6e3e0b3e18da31c5ae
papajo <joshipv2@gmail.com>
2016-03-25 20:40:55 -0400

    signup form user validation using express flash

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var ejs = require('ejs');
 var ejsmate = require('ejs-mate');
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 var app = express();
 
@@ -23,6 +26,13 @@ mongoose.connect(dbpass, function(err){
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "!!!AdamEve!!!"
+}));
+app.use(flash());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('ejs', ejsmate);
 app.set('view engine', 'ejs');
